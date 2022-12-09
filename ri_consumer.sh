1ae81a2c80cef75e083602db2cfff6a8628956ae
Deichmann, Jan Lukas <janlukas.deichmann@draeger.com>
2022-12-09 13:16:47 +0100

    More!

--- ri_consumer.sh
+++ ri_consumer.sh
@@ -7,7 +7,7 @@ export ref_ca=$(pwd)/certs
 export ref_ssl_passwd=dummypass
 
 echo "Starting sdc11073 provider"
-python3 sdc11073/examples/ReferenceTest/reference_device.py &
+python3 sdc11073_git/examples/ReferenceTest/reference_device.py &
 echo "Starting SDCri consumer"
 cd ri && mvn -Pconsumer-tls -Pallow-snapshots exec:java
 test_exit_code=$?
