0d7f75edda8d36e1e57212a30d603ec8bcb33633
Deichmann, Jan Lukas <janlukas.deichmann@draeger.com>
2023-07-21 07:54:56 +0200

    Fix?

--- ri_consumer.sh
+++ ri_consumer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+args=("$@")
+
 ip addr
 echo ref_ip is "${ref_ip}"
 
@@ -10,7 +12,12 @@ export ref_ca=$(pwd)/certs
 export ref_ssl_passwd=dummypass
 
 echo "Starting sdc11073 provider"
+
+if [args[0]="v1.x.y"] then
 python3 sdc11073_git/examples/ReferenceTest/reference_device.py &
+else 
+python3 sdc11073_git/examples/ReferenceTest/reference_provider.py &
+fi
 echo "Starting SDCri consumer"
 cd ri && mvn -Pconsumer-tls -Pallow-snapshots exec:java
 test_exit_code=$?
