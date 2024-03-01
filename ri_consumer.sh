270ef78e5489d5cf6d5a2558d6267a6f4646aec8
a-kleinf <87371692+a-kleinf@users.noreply.github.com>
2024-03-01 09:22:12 +0100

    Adapt pipeline to run again (#1)
    * Test sdc11073 v2.0.1 against SDCri 5.1.1
    
    * Use a-kleinf repo with fixes
    
    * Set environment variables to use ssl in sdc11073 ref consumer
    
    * Use provider with tls
    
    * Add sdc11073 test - reference_consumer
    
    * Fix syntax
    
    * Fix syntax to execute tests
    
    * Run as module
    
    * Add -m
    
    * Don't filter for epr
    
    * Use specific epr for ri provider e
    
    * Use tls matrix
    
    * Use tls matrix and ri 5.0.0
    
    * Only sdc11073 v2
    
    * Really use TLS with ri provider
    
    * Indocate that external SDC Provider is started
    
    * Maximum executions of search services in sdc11073
    
    * Make use of different branch
    
    * Execute tests on special sdc11073 commit - changes only applied to reference tests
    
    * Use draegerwerk repo, sdc11073 master and SDCri 5.1.1
    
    * Parameterize SDCri version in pom.xml
    
    * Try SDCri version matrix
    
    * Fix parameters
    
    * Fix usage of TLS flags
    
    * Remove blank lines

--- ri_consumer.sh
+++ ri_consumer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 args=("$@")
-
-echo arg1 is "${args[0]}"
+echo sdcri-version is "${args[0]}"
+echo flag indicating to use TLS is "${args[1]}"
 
 ip addr
 echo ref_ip is "${ref_ip}"
@@ -10,19 +10,22 @@ echo ref_ip is "${ref_ip}"
 export ref_fac="theFacility"
 export ref_bed="comfyBed"
 export ref_poc="noPoint"
+if [ "${args[1]}" == "true" ]; then
 export ref_ca=$(pwd)/certs
 export ref_ssl_passwd=dummypass
+fi
 
 echo "Starting sdc11073 provider"
 
-if [ "${args[0]}" == "v1.x.y" ]; then
-    python3 sdc11073_git/examples/ReferenceTest/reference_device.py &
-else 
-    python3 sdc11073_git/examples/ReferenceTest/reference_provider.py &
+python3 sdc11073_git/examples/ReferenceTest/reference_provider.py &
+
+if [ "${args[1]}" == "true" ]; then
+echo "Starting SDCri consumer with TLS"
+cd ri && mvn -Dsdcri-version=${args[0]} -Pconsumer-tls -Pallow-snapshots exec:java; ((test_exit_code = $?))
+else
+echo "Starting SDCri consumer without TLS"
+cd ri && mvn -Dsdcri-version=${args[0]} -Pconsumer -Pallow-snapshots exec:java; ((test_exit_code = $?))
 fi
-echo "Starting SDCri consumer"
-cd ri && mvn -Pconsumer-tls -Pallow-snapshots exec:java
-test_exit_code=$?
 
 echo "Terminating sdc11073 provider"
 jobs && kill %1
--- sdc11073_consumer.sh
+++ sdc11073_consumer.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
+args=("$@")
+echo sdcri-version is "${args[0]}"
+echo flag indicating to use TLS is "${args[1]}"
 
 ip addr
 echo ref_ip is "${ref_ip}"
 
-echo "Starting SDCri provider"
-(cd ri && sleep 999999999 | mvn -Pprovider -Pallow-snapshots exec:java) &
-sleep 30
-echo "Starting sdc11073 consumer"
-cd sdc11073_git && python3 -m unittest examples.ReferenceTest.test_reference.Test_Reference.test_client_connects
-test_exit_code=$?
+if [ "${args[1]}" == "true" ]; then
+    export ref_ca=$(pwd)/certs
+    export ref_ssl_passwd=dummypass
+fi
+export ref_search_epr=""
+# ref_discovery_runs defines the maximum executions of searching services via sdc11073 wsdiscovery
+export ref_discovery_runs="20"
+# EXTERNAL_DEVICE_RUNNING="true" indicates unittest in sdc11073 that an external SDC Provider is started
+export EXTERNAL_DEVICE_RUNNING="true"
+
+if [ "${args[1]}" == "true" ]; then
+echo "Starting SDCri provider with TLS"
+(cd ri && sleep 999999999 | mvn -Dsdcri-version=${args[0]} -Pprovider-tls -Pallow-snapshots exec:java) &
+else
+echo "Starting SDCri provider without TLS"
+(cd ri && sleep 999999999 | mvn -Dsdcri-version=${args[0]} -Pprovider -Pallow-snapshots exec:java) &
+fi
+sleep 20
+
+cd sdc11073_git 
+echo "Starting sdc11073 consumer reference_consumer"
+python3 -m examples.ReferenceTest.reference_consumer; ((test_exit_code = $?))
+echo "Starting sdc11073 consumer test_client_connects"
+python3 -m unittest examples.ReferenceTest.test_reference.Test_Reference.test_client_connects; ((test_exit_code = test_exit_code || $?))
 
 echo "Terminating SDCri provider"
 jobs && kill %1
